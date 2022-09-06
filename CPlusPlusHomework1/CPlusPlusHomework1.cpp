c10e2379d67381887e31e72f86f334905cb0e08c
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2022-09-06 12:05:16 +0100

    Done question 4

--- CPlusPlusHomework1/CPlusPlusHomework1.cpp
+++ CPlusPlusHomework1/CPlusPlusHomework1.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-    
+
     //Q1: Variables: Rectangle Info.  Find the perimeter and area of a rectangle, given the length and width. Store each value as a variable, and print the results to the console.
     //Variables
     int Length = 2;
@@ -18,16 +18,16 @@ int main()
     Perimeter = (2 * Length) + (2 * Width);
 
     //Output
-    std::cout << "The length of the rectangle is "; 
+    std::cout << "The length of the rectangle is ";
     std::cout << Length;
-    std::cout <<"!\n";
-    std::cout << "The width of the rectangle is "; 
-    std::cout<< Width;
+    std::cout << "!\n";
+    std::cout << "The width of the rectangle is ";
+    std::cout << Width;
     std::cout << "!\n";
     std::cout << "The perimeter of the rectangle is ";
     std::cout << Perimeter;
     std::cout << "!\n" << "\n";
-    
+
 
     //Q2:Strings: Code Word Locater. Store a codeword as a string variable. Create a longer string variable that contains the codeword. Search for the codeword in the string, 
     //and print out it's position in the string.
@@ -43,7 +43,7 @@ int main()
 
     //Output the position
     Letters += Camouflage.substr(spacePos + 1);
-    std::cout<<"The position of the codeword in the text is: " << spacePos << "\n";
+    std::cout << "The position of the codeword in the text is: " << spacePos << "\n";
 
 
     //Q3:Input: Date of Birth. Ask the player for their Year, Month, and Day of birth. Store each in an appropriate variable, then print them out together.
@@ -52,9 +52,9 @@ int main()
     int BirthMonth;
     int BirthDay;
 
-    
+
     //Ask for year of birth
-    std::cout << "Please enter the year you were born as a whole number."<< "\n";
+    std::cout << "Please enter the year you were born as a whole number." << "\n";
     std::cin >> BirthYear;
 
     //Ask for month of birth
@@ -74,11 +74,64 @@ int main()
 
     //Q4:Selection: Swear Bleeper. Store a "bad" word (it could be anything) in a string variable. Ask the player to enter a single word. Compare if the word they entered is the 
     //same as the bad word, and if it is, print out the word "BLEEP". If it is NOT the same, print out the word the player entered.
+    //Declare variables
+    std::string BadWord = "Fuck";
+    std::string EnteredWord;
+
+    //Ask the player to enter a word
+    std::cout << "Please enter a singular word." << "\n";
+    std::cin >> EnteredWord;
+    //Check if the entered word is bad
+    if (EnteredWord == BadWord)
+    {
+        std::cout << "BLEEP" << "\n";
+
+    }
+    else
+    {
+        std::cout << EnteredWord << "\n";
+
+    }
 
 
     //Q5:Iteration: Bottles of Beer on the Wall. Read an input for a number of bottles of beer to have on the wall. Ask the user if they would like to drink a bottle. 
     //If they do, print the corresponding lyrics to “99 bottles of beer on the wall”, but with the input number of beers on the wall, which is then decreased by 1. 
     //Repeat until they stop drinking or run out of beer.
+    //Declare Variables
+    int BottleNumber;
+    char DrinkBeer;
+
+
+    //Ask the player how many bottles of beer are on the wall
+    std::cin >> BottleNumber;
+
+    //Start the game
+    std::cout << "Do you want to drink a bottle? (Y/N)" << "\n";
+    std::cin >> DrinkBeer;
+    do
+    {
+
+
+
+        if (DrinkBeer == 'Y')
+        {
+            std::cout << BottleNumber << " bottles of beer on the wall, " << BottleNumber << " bottles of beer. Take one down and pass it around, " << BottleNumber - 1 << " bottles of beer on the wall." << "\n";
+            BottleNumber = BottleNumber - 1;
+
+        }
+        else if (DrinkBeer == 'N')
+        {
+            BottleNumber = 0;
+
+        }
+
+
+
+
+    } while (BottleNumber>0);
+    {
+        
+    }
 
 
 }
